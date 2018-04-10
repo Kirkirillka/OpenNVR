930ad960f3e9a2848cb16823789b94d0728933cb
Kirill <kirillkukarcev.gmail.com>
2018-04-10 17:14:01 -0400

    Set to use nginx with nginx_pam_auth module which allows you top user pam local user for HTTP Basic authentication. Also set to user vsftpd pam accounts for similar purpose as above too. Some syntax beatufying took a place here.

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,62 @@
 #/opt/py_cam/web - webserver
 
 
+
+#Extra libs
+apt-get install -f libpam0g-dev
+
+
+
+###--------------------------------------------------------------
+#Installing vsftpd daemon
+apt-get install -f vsftpd
+cp vsftpd.conf /etc
+service vsftpd restart
+###--------------------------------------------------------------
+
+
+
+
+
+###--------------------------------------------------------------
+#Installing nginx server
+#Compiling nginx from sources with ngx_http_auth_pam_module
+olddir=$(pwd)
+cd nginx/nginx-1.12.2
+echo $(pwd)
+./configure --add-module=../ngx_http_auth_pam_module --without-http_rewrite_module
+#make
+#make install
+cd $olddir
+
+
+#Need for www-data nginx to be able to read from /etc/shadow for auth via nginx_pam_auth
+usermod -a -G shadow www-data
+
+cp py_cam /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/py_cam /etc/nginx/sites-enabled
+service nginx restart
+###--------------------------------------------------------------
+
+
+
+###--------------------------------------------------------------
+#Create PyCam specific directories
 mkdir -p /var/opt/py_cam/
 mkdir -p /var/opt/py_cam/backups
 mkdir -p /opt/py_cam/
+mkdir -p /var/www/py_cam
 
+#Copy files
 cp -r scripts /var/opt/py_cam/
 cp -r service_templates /var/opt/py_cam
+cp -r web/static /var/www/py_cam
 cp -r cam /opt/py_cam/
 cp -r web /opt/py_cam/
 cp -r led /opt/py_cam
 cp __init__.py /opt/py_cam
+###--------------------------------------------------------------
+
+
+
 
