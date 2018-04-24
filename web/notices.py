3e3f6c26f5514995c62050a3b5d77925465bb7ed
Kirill <kirillkukarcev.gmail.com>
2018-04-24 17:58:20 -0400

    New notices for alertify

--- web/notices.py
+++ web/notices.py
@@ -13,3 +13,23 @@ UPDATE_CONFIG={'type':'info',
 
 ERROR={'type':'error',
        'message':'Some error occured. Contact to developers to send bug info.'}
+
+
+USER_ADD_FAILED={'type':'error',
+                  'message':'Unable to add new user. Please, check supplied data.'}
+
+USER_ADD_SUCCESS={'type':'success',
+                  'message':'A new user was added onto system. Now he is allowed to authenticate.'}
+
+USER_UPDATE_FAILED={'type':'error',
+                    'message':'Cannot update user with supplied data'}
+
+USER_UPDATE_SUCCESS={'type':'success',
+                     'message':'User\' configuration updated.'}
+
+
+USER_DELETE_FAILED={'type':'error',
+                     'message':'Cannot delete this user.'}
+
+USER_DELETE_SUCCESS={'type':'success',
+                     'message':'User was deleted.'}
