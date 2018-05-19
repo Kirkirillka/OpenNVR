2d618d228144731017be23e4196d61661873772f
Kirill <kirillkukarcev.gmail.com>
2018-05-19 08:59:21 -0400

    Unnesessary import deleted

--- web/auth/database.py
+++ web/auth/database.py
@@ -14,5 +14,4 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    import web.auth.models
     Base.metadata.create_all(bind=engine)
