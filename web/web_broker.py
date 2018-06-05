5d23ae2c64ea6f0b25808f0ad8b46e4c08fd067d
Kirill <kirillkukarcev.gmail.com>
2018-06-05 16:47:24 -0400

    clean ups

--- web/web_broker.py
+++ web/web_broker.py
@@ -4,23 +4,20 @@ import os
 sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath("."))
 
-from flask import Flask, send_from_directory, render_template, jsonify, request, redirect, url_for, Response, \
-    get_flashed_messages, flash,abort
-from flask_security import login_required, current_user, roles_required, Security, SQLAlchemySessionUserDatastore
-from flask_security.utils import hash_password,verify_and_update_password
-
-
+from flask import Flask, send_from_directory, render_template, jsonify, request, redirect, url_for, Response, flash,abort
+from flask_security import current_user, Security, SQLAlchemySessionUserDatastore,login_required,roles_required
+from flask_security.utils import hash_password,verify_and_update_password,logout_user
 from web.auth.database import db_session, init_db
 from web.auth.models import User, Role
 from led import Broker
-from web.utils import ServiceManager,MessageQueue,SourceManager,ConfigManager
-# from web.auth.database import init_security, hash_password
-
-# from web.tasks import make_celery
+from web.utils import ServiceManager,MessageQueue,SourceManager,ConfigManager,BackupManager
 from celery import Celery
 from web.notices import *
 
+
+
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = os.urandom(128)
 app.config['SECURITY_PASSWORD_HASH'] = 'pbkdf2_sha512'
 app.config['SECURITY_PASSWORD_SALT'] = b'12412126yyyg12t12125b125m125m21ffvdvsrbwenwey'
@@ -29,20 +26,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth/users.db'
 # Celery configuration
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
+# Setup Flask-Redis for flashing messages :)
+app.config['REDIS_URL']='redis://localhost:6379'
 # Initialize Celery
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
-
-led = Broker()
-
-
-
-
-
-#Setup Flask-Redis for flashing messages :)
-app.config['REDIS_URL']='redis://localhost:6379'
-queue=MessageQueue(app)
-
 
 
 class UserManager():
@@ -174,52 +162,43 @@ class UserManager():
         hive.commit()
         return 0
 
+    def pwd_change(self,user,data):
+        pwd=data.get('password')
+        confirm=data.get('check')
+        verified=verify_and_update_password(confirm,user)
+        if verified:
+            user.password=hash_password(pwd)
+        return verified
 
 
-
+queue=MessageQueue(app)
 services=ServiceManager()
 cam_server=SourceManager()
 configurator=ConfigManager()
 userManager=UserManager(app)
-
-
-@celery.task()
-def addCam(source):
-    if cam_server.add_cam(source):
-        return True
-    return False
-
-
-@celery.task()
-def delCam(source):
-    if cam_server.del_cam(source):
-        return True
-    return False
-
-
-@celery.task()
-def updateConf(data):
-    configurator.update(data)
-    return True
+backupManager=BackupManager()
+led = Broker()
 
 
 @app.route("/")
 def index():
     led.show_success()
+
     return render_template("index.html", current_user=current_user)
 
 
 @app.route("/settings", methods=['GET'])
-# @login_required
-# @roles_required('admin')
+@login_required
+@roles_required('admin')
 def settings():
+    led.show_success()
     if current_user.has_role('admin'):
         return render_template('settings.html', current_user=current_user)
     return redirect(url_for('index'))
 
 
 @app.route("/config/<action>", methods=['GET', 'POST'])
-# @login_required
+@login_required
 def config(action):
     if request.method == 'GET':
 
@@ -231,20 +210,22 @@ def config(action):
             if action == 'reinitialize':
                 result = cam_server.update_db()
                 if result:
+                    led.show_success()
                     return jsonify(REINITILAZE)
                 else:
+                    led.show_error()
                     return jsonify(ERROR)
-
         return Response()
 
     if request.method == 'POST':
         if current_user.has_role('admin'):
             if action == 'update':
                 data = request.json
-                # updateConf.apply_async([data, ])
                 result = configurator.update(data)
                 if result:
+                    led.show_success()
                     return jsonify(UPDATE_CONFIG)
+                led.show_error()
                 return jsonify(ERROR)
             return redirect(url_for('index'))
 
@@ -252,68 +233,78 @@ def config(action):
 @app.route('/users/<action>', methods=['GET', 'POST'])
 def users(action='get'):
     if request.method == 'GET':
-
-        user_list = [{'name': 'Kirill', 'enabled': True}, {'name': 'Oleg', 'enabled': False},
-                     {'name': 'Max', 'enabled': True}]
-
         if current_user.has_role('user'):
             if action == 'current':
+                led.show_success()
                 return jsonify({'email': current_user.email,
                                 'roles': [role.name for role in current_user.roles]
                                 })
 
         if current_user.has_role('admin'):
             if action == 'get':
-
+                led.show_success()
                 return jsonify(userManager.users)
-
+        led.show_error()
         return abort(401)
 
     if request.method == 'POST':
-
         if current_user.has_role('admin'):
             if action == 'add':
-                data=request.json
-                result=userManager.add_user(data)
-                response =USER_ADD_FAILED
+                data = request.json
+                result = userManager.add_user(data)
+                response = USER_ADD_FAILED
                 if not result:
-                    response=USER_ADD_SUCCESS
+                    response = USER_ADD_SUCCESS
+                    led.show_success()
                 return jsonify(response)
 
             if action == 'del':
-                data=request.json
-                id=data['id']
+                data = request.json
+                id = data['id']
                 result = userManager.del_user(id)
                 response = USER_DELETE_FAILED
                 if not result:
                     response = USER_DELETE_SUCCESS
+                    led.show_success()
                 return jsonify(response)
 
-
             if action == 'update':
-                update_data=request.json
-                id=update_data.get('id',None)
+                update_data = request.json
+                id = update_data.get('id',None)
                 result = userManager.update(id,update_data)
                 response = USER_UPDATE_FAILED
                 if not result:
                     response = USER_UPDATE_SUCCESS
+                    led.show_success()
                 return jsonify(response)
 
+            if action == 'pwd_change':
+                update_data=request.form
+                result = userManager.pwd_change(current_user, update_data)
+                response = USER_PASSWORD_CHANGE_FAILED
+                if result:
+                    response = USER_PASSWORD_CHANGE_SUCCESS
+                    led.show_success()
+                    logout_user()
+                return redirect(url_for('index'))
+
             if action == 'disable':
-                data=request.json
-                id=data['id']
-                result=userManager.deactivate(id)
+                data = request.json
+                id = data['id']
+                result = userManager.deactivate(id)
                 response = USER_UPDATE_FAILED
                 if not result:
+                    led.show_success()
                     response = USER_UPDATE_SUCCESS
                 return jsonify(response)
 
             if action == 'enable':
-                data=request.json
-                id=data['id']
-                result=userManager.activate(id)
+                data = request.json
+                id = data['id']
+                result = userManager.activate(id)
                 response = USER_UPDATE_FAILED
                 if not result:
+                    led.show_success()
                     response = USER_UPDATE_SUCCESS
                 return jsonify(response)
 
@@ -321,77 +312,81 @@ def users(action='get'):
 
 
 @app.route("/cam/<action>", methods=['GET', 'POST'])
-# @login_required
+@login_required
 def sources(action):
     if request.method == 'GET':
         if current_user.has_role('user'):
             flash('user')
             if action == 'all':
+                led.show_success()
                 return jsonify(cam_server.availiable)
             if action == 'free':
+                led.show_success()
                 return jsonify(cam_server.free)
             if action == 'enabled':
+                led.show_success()
                 return jsonify(cam_server.enabled)
 
     if request.method == 'POST':
         if current_user.has_role('admin'):
             if action == 'add':
                 src = request.json
-                # result = addCam.delay(src)
+                led.show_success()
                 result = cam_server.add_cam(src)
                 return jsonify(CAM_ADDED)
-                # if result.wait():
-                #    return jsonify(dict(success='True'))
+
             if action == 'del':
                 src = request.json
+                led.show_success()
                 result = cam_server.del_cam(src['name'])
                 return jsonify(CAM_DELETED)
-                # result=delCam.delay(src['name'])
-                # if result.wait():
-                #    return jsonify(dict(success='True'))
-                # return jsonify(dict(success='False'))
 
+
+            return jsonify(NO_ACTION)
 
 
 
 @app.route("/services/",methods=['GET',])
 def service_list():
+    led.show_success()
     return jsonify(configurator.hive['services'])
+
+
+@app.route("/services/restart",methods=['POST'])
+def service_restart():
+    result = services.restart()
+    if result:
+        led.show_success()
+        return jsonify(SERVICE_RESTART_SUCCESS)
+    led.show_error()
+    return jsonify(SERVICE_RESTART_FAILED)
+
+
 
 @app.route("/service",methods=['POST',])
 def service_do():
-    action=request.json['action']
+    action = request.json['action']
     service_name = request.json['name']
     result='False'
-    if action=='on':
+    if action == 'on':
+            led.show_success()
             result=services.start(service_name)
-    if action=='off':
+    if action == 'off':
+            led.show_success()
             result=services.stop(service_name)
     return jsonify({'success':result})
 
+
 @app.route('/message/<email>/',methods=['GET',])
 def message(email):
+    led.show_success()
     if request.method=='GET':
-        if email=='1' or  current_user.is_authenticated:
-            return jsonify(
-                dict(
-                    [(i,r) for i,r in  enumerate(queue.pop(current_user.email))
-                     ]
-                )
-            )
+        if current_user.is_authenticated:
+            led.show_success()
+            return jsonify(dict([(i,r) for i,r in  enumerate(queue.pop(current_user.email))]))
+        led.show_error()
         return jsonify([])
 
 
-
-
-@app.route('/static/<path>')
-def get_static(path):
-    led.show_error()
-    return send_from_directory('static', path)
-
-
-
-
-
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True)
+    app.run('0.0.0.0')
